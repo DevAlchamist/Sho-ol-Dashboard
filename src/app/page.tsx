e8d3582e33d4498a674f4e5205c58d2af4df6dd3
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-03-05 22:42:08 +0530

    FIRST

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HomePage from "@/components/HomePage";
+import SignUp from "@/components/SignUp";
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box className="bg-[#F3F4FF]">
+      <HomePage />
+    </Box>
+  );
+}
